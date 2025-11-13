0e2974fb882c1594f433e5c6d519c399af5e2bb1
Kris <kriszenyte@gmail.com>
2025-11-13 20:45:05 +0200

    fix: update base path

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: './',
+  base: '/zenyte-map/',
   build: {
-    outDir: 'dist',
+    outDir: 'docs',
     assetsDir: 'assets',
     rollupOptions: {
       input: {
